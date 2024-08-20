f6d69d5e0071e644f4b475303d9d4b1219c89964
KanataIkumi <i.kanata0531@gmail.com>
2024-08-20 21:21:05 +0900

    send state to a different screen

--- lib/Navigator/first_screen.dart
+++ lib/Navigator/first_screen.dart
@@ -7,26 +7,47 @@ void main() {
   ));
 }
 
-class FirstScreen extends StatelessWidget {
+class FirstScreen extends StatefulWidget {
   const FirstScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _FirstScreenState();
+  }
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  int _number = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('FirstScreen'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Go to Second Screen'),
-          onPressed: () {
-            final NavigatorState navigator = Navigator.of(context);
-            final route =
-                MaterialPageRoute(builder: (context) => const SecondScreen());
-            navigator.push(route);
-          },
+        appBar: AppBar(
+          title: const Text('FirstScreen'),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('number = $_number'),
+              ElevatedButton(
+                child: const Text('Go to Second Screen'),
+                onPressed: () async {
+                  final newNumber = await Navigator.of(context).push<int>(
+                    MaterialPageRoute(
+                      builder: (_) => SecondScreen(number: _number),
+                    ),
+                  );
+                  setState(() {
+                    if (newNumber != null) {
+                      _number = newNumber;
+                    }
+                  });
+                },
+              ),
+            ],
+          ),
+        ));
   }
 }
--- lib/Navigator/second_screen.dart
+++ lib/Navigator/second_screen.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
 
 class SecondScreen extends StatelessWidget {
-  const SecondScreen({super.key});
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: const Text('SecondScreen'),
-    ),
-    body: Center(
-      child: ElevatedButton(
-        child: const Text('戻る'),
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-      ),
-    ),
-  );
-}
-}
+  const SecondScreen({super.key, required this.number});
 
+  final int number;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('SecondScreen'),
+        ),
+        body: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            ElevatedButton(
+              child: const Text('increment'),
+              onPressed: () {
+                Navigator.of(context).pop(number + 1);
+              },
+            ),
+            ElevatedButton(
+              child: const Text('decrement'),
+              onPressed: () {
+                Navigator.of(context).pop(number - 1);
+              },
+            ),
+          ]),
+        ));
+  }
+}
