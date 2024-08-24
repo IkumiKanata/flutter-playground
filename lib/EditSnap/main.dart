b1d0f4dcc73c38d506afeeda36dac90e8f00cde5
KanataIkumi <i.kanata0531@gmail.com>
2024-08-24 23:19:25 +0900

    initial for editing snap

---
+++ lib/EditSnap/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_playground/EditSnap/start_screen.dart';
+
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'EditSnap',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const StartScreen(),
+    );
+  }
+}
